379e628837c68dbb475a4310e7ded17e19c40ddd
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-10 21:32:24 +0500

    elif

--- Crash-Course-on-Python/Module2/ConditionalsExm.py
+++ Crash-Course-on-Python/Module2/ConditionalsExm.py
@@ -11,7 +11,7 @@ def hint_username(username):
             print("Valid username")
 
 #22
-def hint_username(username):
+def hint_username2(username):
     if len(username) < 3:
         print("Invalid username. Must be at least 3 characters long")
     elif len(username) > 15:
