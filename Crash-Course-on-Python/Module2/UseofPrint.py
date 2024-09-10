efad58517165ee07d6a327fa63d744b9fbe13e49
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-10 19:12:05 +0500

    Printing Strings

---
+++ Crash-Course-on-Python/Module2/UseofPrint.py
@@ -0,0 +1,14 @@
+# The following 5 lines assign strings to a list of variables.
+salutation = "Dr."
+first_name = "Prisha"
+middle_name = "Jai"
+last_name = "Agarwal"
+suffix = "Ph.D."
+ 
+print(salutation + " " + first_name + " " + middle_name + " " + last_name + ", " + suffix) 
+# The comma as a string ", " adds the conventional use of a comma plus a 
+# space to separate the last name from the suffix.
+ 
+# Alternatively, you could use commas in place of the + connector:
+print(salutation, first_name, middle_name, last_name,",", suffix)
+# However, you will find that this produces a space before a comma within a string.
