220fedd1b0819d1f1ce38bdf1ed6f45a58aeb732
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-10 21:26:18 +0500

    Branching

--- Crash-Course-on-Python/Module2/ConditionalsExm.py
+++ Crash-Course-on-Python/Module2/ConditionalsExm.py
@@ -1,1 +1,20 @@
 print("Yellow" > "Cyan" and "Magenta" > "Brown")
+
+#2
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    else:
+        if len(username) > 15:
+            print("Invalid username. Must be at most 15 characters long")
+        else:
+            print("Valid username")
+
+#22
+def hint_username(username):
+    if len(username) < 3:
+        print("Invalid username. Must be at least 3 characters long")
+    elif len(username) > 15:
+        print("Invalid username. Must be at most 15 characters long")
+    else:
+        print("Valid username")
