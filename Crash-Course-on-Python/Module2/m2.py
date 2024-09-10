f2c865b660e4bc3108e0009bd272e78058642afd
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-10 19:40:40 +0500

    Module 2

---
+++ Crash-Course-on-Python/Module2/m2.py
@@ -0,0 +1,8 @@
+print("2 + 2 = " + str(2 + 2))
+
+bill = 47.28 # Assign "bill" variable with bill amount
+tip = bill * 0.15 # Multiply by stated tip rate 
+total = bill + tip # Sum the "total" of the "bill" and "tip"
+share = total/2 # Divide "total" by number of friends dining
+print("Each person needs to pay:" + str(share)) # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data types
