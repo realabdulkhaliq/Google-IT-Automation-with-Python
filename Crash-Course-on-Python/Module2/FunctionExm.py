aef6a3f30d622cd30ce6475d4e307531ee485155
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-10 20:00:34 +0500

    Area of Triangle

--- Crash-Course-on-Python/Module2/FunctionExm.py
+++ Crash-Course-on-Python/Module2/FunctionExm.py
@@ -2,3 +2,11 @@ def greeting(name):
     print("Hello, " + name)
 
 greeting("AK")
+
+
+def area_triangle(base, height):
+    return base*height/2
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+sum = area_a + area_b
+print("The sum of both areas is: " + str(sum))
