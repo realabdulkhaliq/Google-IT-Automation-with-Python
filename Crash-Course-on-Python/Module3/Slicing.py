8c6335cbbf904952a48c81c054fb2845b7e2b76f
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-11 22:05:43 +0500

    Formate a Phone Number

--- Crash-Course-on-Python/Module3/Slicing.py
+++ Crash-Course-on-Python/Module3/Slicing.py
@@ -3,3 +3,8 @@ print(string1[0])   # Prints “G”
 print(string1[4:8]) # Prints “ting”
 print(string1[11:]) # Prints “Earthlings”
 print(string1[:5])  # Prints “Greet”
+
+# An optional way to slice an index is by the stride argument, indicated by using a double colon.
+
+print(string1[0::2])  # Prints “Getns atlns”
+print(string1[::-1])  # Prints “sgnilhtraE ,sgniteerG”
---
+++ Crash-Course-on-Python/Module3/Slicing_Formate_a_Phone_Number.py
@@ -0,0 +1,8 @@
+def format_phone(phonenum):
+    area_code = "(" + phonenum[:3] + ")"
+    exchange = phonenum[3:6]
+    line = phonenum[-4:]
+    return area_code + " " + exchange + "-" + line
+
+
+print(format_phone("2025551212")) # Outputs: (202) 555-1212
