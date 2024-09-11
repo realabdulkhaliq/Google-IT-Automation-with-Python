0d71c74f63757357fa61e8c7bb8cebe6d4dfd8a8
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-11 21:58:59 +0500

    Slicing Basic

---
+++ Crash-Course-on-Python/Module3/Slicing.py
@@ -0,0 +1,5 @@
+string1 = "Greetings, Earthlings"
+print(string1[0])   # Prints “G”
+print(string1[4:8]) # Prints “ting”
+print(string1[11:]) # Prints “Earthlings”
+print(string1[:5])  # Prints “Greet”
