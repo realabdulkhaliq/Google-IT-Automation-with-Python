b8f10722e8c7d8cf0ed993a37d3b6c63e898468d
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-12 21:30:39 +0500

    Factorial examples

--- Crash-Course-on-Python/Module3/Recursion_factorial.py
+++ Crash-Course-on-Python/Module3/Recursion_factorial.py
@@ -23,3 +23,17 @@ def factorial(n):
   return result
 
 factorial(4)
+
+def factorial3(n):
+    result = n
+    start = n
+    n -= 1
+    while n>0: # The while loop should execute as long as n is greater than 0
+        result *= n # Multiply the current result by the current value of n
+        n -= 1 # Decrement the appropriate variable by -1
+    return result
+
+
+print(factorial3(3)) # Should print 6
+print(factorial3(9)) # Should print 362880
+print(factorial3(1)) # Should print 1
