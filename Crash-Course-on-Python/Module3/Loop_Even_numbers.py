7d143040acd93adce5eb44ca1346b52417276891
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-12 19:08:47 +0500

    Even Number with for loop

---
+++ Crash-Course-on-Python/Module3/Loop_Even_numbers.py
@@ -0,0 +1,16 @@
+# This for loop iterates through the numbers 0 to 6. The if statement
+# uses the modulo operator to test if the "x" variable is divisible by
+# 2. If True, the if statement will print the value of "x" and exit
+# back into the for loop for the next iteration of "x". Since no 
+# incremental value is specified in the range() parameters, the default
+# increment is +1. 
+
+for x in range(7):
+    if x % 2 == 0:
+        print(x)
+
+# The loop should print 0, 2, 4, 6
+
+# As a list comprehension:
+even_numbers = [x for x in range(7) if x % 2 == 0]
+print(even_numbers)
