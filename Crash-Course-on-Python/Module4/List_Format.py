eeaf1c21067bbaeec5d72408306160f73833f728
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-14 17:39:58 +0500

    List format

---
+++ Crash-Course-on-Python/Module4/List_Format.py
@@ -0,0 +1,19 @@
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:
+  chars += len(animal)
+
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+
+
+winners = ["Ashley", "Dylan", "Reese"]
+for index, person in enumerate(winners):
+  print("{} - {}".format(index + 1, person))
+
+
+def full_emails(people):
+  result = []
+  for email, name in people:
+    result.append("{} <{}>".format(name, email))
+  return result
+print(full_emails([("alex@example.com", "Alex Diego"), ("shay@example.com", "Shay Brandt")]))
