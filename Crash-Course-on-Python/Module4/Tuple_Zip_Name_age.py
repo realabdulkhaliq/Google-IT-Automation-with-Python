01f89147b7ad9458be8587449859ab8fc9769145
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-14 17:41:59 +0500

    Tuple zip name and age

---
+++ Crash-Course-on-Python/Module4/Tuple_Zip_Name_age.py
@@ -0,0 +1,11 @@
+# Two lists
+names = ["Alice", "Bob", "Charlie"]
+ages = [25, 30, 35]
+
+# Use zip to combine the lists
+combined = zip(names, ages)
+
+# Convert the zip object to a list to print the result
+print(list(combined))
+
+# Outputs: [('Alice', 25), ('Bob', 30), ('Charlie', 35)]
