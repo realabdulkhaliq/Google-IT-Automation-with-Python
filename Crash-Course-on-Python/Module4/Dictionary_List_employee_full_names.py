11dee89b42e7145fdd947fd0de5343ed6da4db91
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-14 17:37:13 +0500

    Dictionary + List print employee full name

---
+++ Crash-Course-on-Python/Module4/Dictionary_List_employee_full_names.py
@@ -0,0 +1,32 @@
+# This function receives a dictionary, which contains common employee 
+# last names as keys, and a list of employee first names as values. 
+# The function generates a new list that contains each employees’ full
+# name (First_name Last_Name). For example, the key "Garcia" with the 
+# values ["Maria", "Hugo", "Lucia"] should be converted to a list 
+# that contains ["Maria Garcia", "Hugo Garcia", "Lucia Garcia"].
+
+
+def list_full_names(employee_dictionary):
+    # Initialize the "full_names" variable as a list data type using
+    # empty [] square brackets.  
+    full_names = []
+
+    # The outer for loop iterates through each "last_name" key and 
+    # associated "first_name" values, in the "employee_dictionary" items.
+    for last_name, first_names in employee_dictionary.items():
+
+        # The inner for loop iterates over each "first_name" value in 
+        # the list of "first_names" for one "last_name" key at a time.
+        for first_name in first_names:
+
+            # Append the new "full_names" list with the "first_name" value
+            # concatenated with a space " ", and the key "last_name". 
+            full_names.append(first_name+" "+last_name)
+            
+    # Return the new "full_names" list once the outer for loop has 
+    # completed all iterations. 
+    return(full_names)
+
+
+print(list_full_names({"Ali": ["Muhammad", "Amir", "Malik"], "Devi": ["Ram", "Amaira"], "Chen": ["Feng", "Li"]}))
+# Should print ['Muhammad Ali', 'Amir Ali', 'Malik Ali', 'Ram Devi', 'Amaira Devi', 'Feng Chen', 'Li Chen']
