edd702889240535e688a4aeacedfe5042d1a0b3e
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-15 19:02:22 +0500

    String Comperison with and operator

---
+++ Crash-Course-on-Python/Module2/String_Comperison_and_operator.py
@@ -0,0 +1,9 @@
+print("Nairobi" < "Milan" and "Nairobi" > "Hanoi")
+
+
+# In this next example, "Nairobi" < "Milan" and "Nairobi" > "Hanoi", 
+# the and logical operator is connecting two string comparison statements. 
+# You learned previously that using the greater than and less than operators 
+# on strings will test the alphabetical order (technically Unicode values) of the strings. 
+# So, this complex comparison is checking if "Nairobi" is alphabetized before "Milan" (False) 
+# AND after "Hanoi" (True). 
