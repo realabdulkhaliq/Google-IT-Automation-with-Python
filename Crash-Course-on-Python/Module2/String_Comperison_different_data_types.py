fb4d60943ccdd123023df83f74b7c86519fa3336
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-15 19:02:33 +0500

    String Comperison of different data types

---
+++ Crash-Course-on-Python/Module2/String_Comperison_different_data_types.py
@@ -0,0 +1,9 @@
+# This last example illustrates the result of trying to compare two
+# items of different data types using the less than < operator. The 
+# greater than > and less than operators < cannot be used to compare
+# two different data types. 
+print("Five" < 6)
+'''
+Error on line 1:
+    print("Five" < 6)
+TypeError: '<' not supported between instances of 'str' and 'int' '''
