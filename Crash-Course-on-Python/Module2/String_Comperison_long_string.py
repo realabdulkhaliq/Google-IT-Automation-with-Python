6c344c17df97410e3aa740e52fdcdb57e5b97012
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-15 19:02:43 +0500

    String Comperison of  long strings

---
+++ Crash-Course-on-Python/Module2/String_Comperison_long_string.py
@@ -0,0 +1,12 @@
+# Use the Unicode chart in Part 2 to determine if the Unicode values of 
+# the first letters of each string are higher, lower, or equal to one
+# another. 
+
+
+var1 = "my computer" >= "my chair"
+var2 = "Spring" <= "Winter"
+var3 = "pineapple" >= "pineapple"
+ 
+print("Is \"my computer\" greater than or equal to \"my chair\"? Result: ", var1)
+print("Is \"Spring\" less than or equal to \"Winter\"? Result: ", var2)
+print("Is \"pineapple\" less than or equal to \"pineapple\"? Result: ", var3)
