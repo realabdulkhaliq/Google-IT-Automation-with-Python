1b5c585c4f879bb87ccca580da7b11001034e3b1
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-09-15 17:52:41 +0500

    String counts words

---
+++ Crash-Course-on-Python/Module4/String_count_words.py
@@ -0,0 +1,17 @@
+# This function accepts a string variable "data_field".  
+def count_words(data_field):
+
+    # Splits the string into individual words. 
+    split_data = data_field.split()
+  
+    # Then returns the number of words in the string using the len()
+    # function. 
+    return len(split_data)
+    
+    # Note that it is possible to combine the len() function and the 
+    # .split() method into the same line of code by inserting the 
+    # data_field.split() command into the the len() function parameters.
+
+# Call to the function
+print(count_words("Catalog item 3523: Organic raw pumpkin seeds in shell"))
+# Output should be 9
