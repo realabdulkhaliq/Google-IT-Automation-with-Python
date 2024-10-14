313bbfb539001a6094a7921e84dfc781adbf895f
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-10-14 19:46:05 +0500

    Areas calculation

---
+++ Using-Python-to-Interact-with-the-Operating-System/Module1/areas.py
@@ -0,0 +1,10 @@
+import math
+
+def triangle(base, height):
+    return 0.5 * base * height
+
+def rectangle(length, width):
+    return length * width
+
+def circle(radius):
+    return math.pi * radius ** 2
