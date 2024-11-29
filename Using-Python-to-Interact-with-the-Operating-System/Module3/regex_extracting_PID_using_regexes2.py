bd31051dabc5e5ea8a1faa787fa65bbc6a597e08
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-11-29 15:51:24 +0500

    Extracting PID using Regex 2

---
+++ Using-Python-to-Interact-with-the-Operating-System/Module3/regex_extracting_PID_using_regexes2.py
@@ -0,0 +1,13 @@
+import re
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+result = re.search(regex, "A completely different string that also has numbers [34567]")
+result = re.search(regex, "99 elephants in a [cage]")
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]"
+    result = re.search(regex, log_line)
+    if result is None:
+        return ""
+    return result[1]
+print(extract_pid(log))
