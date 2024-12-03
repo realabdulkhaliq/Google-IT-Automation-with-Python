5460cb4568bbcd21b25ac5351d41745d1ace92a0
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-12-03 06:48:48 +0500

    Advanced Subprocess Management

---
+++ Using-Python-to-Interact-with-the-Operating-System/Module4/advanced_subprocess_management.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+my_env = os.environ.copy()
+my_env["PATH"] = os.pathsep.join(["/opt/myapp/", my_env["PATH"]])
+
+result = subprocess.run(["myapp"], env=my_env)
+
+# This modifiy the path of PATH env by adding new directory
