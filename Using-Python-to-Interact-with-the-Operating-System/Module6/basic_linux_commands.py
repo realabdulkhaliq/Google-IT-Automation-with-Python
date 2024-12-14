4d7324fac61a5b4d408aee7083f98e8bf2aa2c24
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-12-14 14:43:37 +0500

    Basic Linux Commands

--- Using-Python-to-Interact-with-the-Operating-System/Module6/basic_linux_commands.py
+++ Using-Python-to-Interact-with-the-Operating-System/Module6/basic_linux_commands.py
@@ -7,26 +7,4 @@
 # #Output:
 # #-rw-rw-r-- 1 user user   0 Mai 22 14:22 myfile.txt
 # #-rw-rw-r-- 1 user user 192 Mai 22 14:18 spider.txt
-# /mynewdir$ ls -la
-# #Output:
-# #total 12
-# #drwxr-xr-x  2 user user  4096 Mai 22 14:17 .
-# #drwxr-xr-x 56 user user 12288 Mai 22 14:17 ..
-# #-rw-rw-r--  1 user user     0 Mai 22 14:22 myfile.txt
-# #-rw-rw-r--  1 user user   192 Mai 22 14:18 spider.txt
-# /mynewdir$ mv myfile.txt emptyfile.txt
-# /mynewdir$ cp spider.txt yetanotherfile.txt
-# /mynewdir$ ls -l
-# #Output:
-# #total 8
-# #-rw-rw-r-- 1 user user   0 Mai 22 14:22 emptyfile.txt
-# #-rw-rw-r-- 1 user user 192 Mai 22 14:18 spider.txt
-# #-rw-rw-r-- 1 user user 192 Mai 22 14:23 yetanotherfile.txt
-# /mynewdir$ rm *
-# /mynewdir$ ls -l
-# #total 0
-# /mynewdir$ cd ..
-# rmdir mynewdir/
-# ls mynewdir
-# #ls: cannot access 'mynewdir': No such file or directory
 
