92ee93a686e0078ca1ce01e8fdb49286b7d195ac
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-12-16 09:07:04 +0500

    Advanced Basic Linux Commands test

--- Using-Python-to-Interact-with-the-Operating-System/Module6/basic_linux_commands.py
+++ Using-Python-to-Interact-with-the-Operating-System/Module6/basic_linux_commands.py
@@ -14,3 +14,11 @@
 # #drwxr-xr-x 56 user user 12288 Mai 22 14:17 ..
 # #-rw-rw-r--  1 user user     0 Mai 22 14:22 myfile.txt
 # #-rw-rw-r--  1 user user   192 Mai 22 14:18 spider.txt
+# /mynewdir$ mv myfile.txt emptyfile.txt
+# /mynewdir$ cp spider.txt yetanotherfile.txt
+# /mynewdir$ ls -l
+# #Output:
+# #total 8
+# #-rw-rw-r-- 1 user user   0 Mai 22 14:22 emptyfile.txt
+# #-rw-rw-r-- 1 user user 192 Mai 22 14:18 spider.txt
+# #-rw-rw-r-- 1 user user 192 Mai 22 14:23 yetanotherfile.txt
