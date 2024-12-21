17d4fdc88788c8c3d91bbeb629eb4f6cb91e7f7f
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2024-12-21 09:06:16 +0500

    For loop using shell script

--- Using-Python-to-Interact-with-the-Operating-System/Module6/story.sh
+++ Using-Python-to-Interact-with-the-Operating-System/Module6/story.sh
@@ -1,1 +1,4 @@
-for i in $(cat story.txt); do B=`echo -n "${i:0:1}" | tr "[:lower:]" "[:upper:]"`; echo -n "${B}${i:1} "; done; echo -e "\n"
+for i in $(cat story.txt); 
+do B=`echo -n "${i:0:1}" | tr "[:lower:]" "[:upper:]"`; 
+echo -n "${B}${i:1} "; 
+done; echo -e "\n"
