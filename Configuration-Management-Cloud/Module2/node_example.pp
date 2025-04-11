e4e97ec9d672bc0ea454a730e958192e6594a799
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-11 21:49:28 +0500

    Node Web Server

--- Configuration-Management-Cloud/Module2/node_example.pp
+++ Configuration-Management-Cloud/Module2/node_example.pp
@@ -8,3 +8,10 @@ node default {
 The command node default installs the sudo and ntp classes on all default nodes. The sudo class is installed with its default parameters, because no parameters are specified. The ntp class is installed with an additional parameter, indicated by servers => ['ntp1.example.com', 'ntp2.example.com'].
 
 
+node webserver.example.com {
+  class { 'sudo': }
+  class { 'ntp':
+        servers => ['ntp1.example.com', 'ntp2.example.com']
+  }
+  class { 'apache': }
+}
